84179699bc09d4f8e46a6d3a4c75f41a98f80ef6
frank <fugangkuang4@gmail.com>
2026-08-31 22:05:34 +0800

    fix: 修改detr训练配置

--- detr/config.py
+++ detr/config.py
@@ -17,12 +17,12 @@ LOG_DIR = ROOT_DIR / "logs"
 LR = 1e-4  # 学习率
 BACKBONE_LR = 1e-5  # 主干网络学习率
 
-BATCH_SIZE = 2  # 批大小
+BATCH_SIZE = 8  # 批大小
 NUM_CLASSES = 20  # VOC20 类，不包括 no-object
 NUM_QUERIES = 100
-D_MODEL = 128  # 模型输入维度
+D_MODEL = 256  # 模型输入维度
 N_HEAD = 4  # 多头注意力头数
 N_LAYER = 4  # 编码器层数
-D_FF = 1024 # 前馈层网络维度
+D_FF = 2048 # 前馈层网络维度
 DROPOUT = 0.1  # 注意力权重dropout概率
-EPOCHS = 200
+EPOCHS = 320
