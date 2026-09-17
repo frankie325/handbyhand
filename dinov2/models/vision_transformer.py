6413dd46cc389b23d982409161bc25618a98f982
kuangfg <1125303286@qq.com>
2026-09-17 17:44:06 +0800

    feat: 添加dinov2手写代码

---
+++ dinov2/models/vision_transformer.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+from ..layers import PatchEmbed
+
+
+class DinoVisionTransformer(nn.Module):
+    def __init__(
+        self,
+        img_size=224,
+        patch_size=16,
+        in_chans=3,
+        embed_dim=768,
+        depth=12,
+        num_heads=12,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        ffn_bias=True,
+        proj_bias=True,
+        drop_path_rate=0.0,
+        drop_path_uniform=False,
+        init_values=None,  # for layerscale: None or 0 => no layerscale
+        embed_layer=PatchEmbed,
+        act_layer=nn.GELU,
+        block_fn=Block,
+        ffn_layer="mlp",
+        block_chunks=1,
+        num_register_tokens=0,
+        interpolate_antialias=False,
+        interpolate_offset=0.1,
+        channel_adaptive=False,
+    ):
+        super().__init__()
+
+    def forward_features_list(self, x_list, masks_list):
+        # x_list.shape = (2*B, 3, 224, 224)
+        pass
+
+    def forward(self, *args, is_training=False, **kwargs):
+        ret = self.forward_features(*args, **kwargs)
+        if is_training:
+            return ret
+        else:
+            return self.head(ret["x_norm_clstoken"])
